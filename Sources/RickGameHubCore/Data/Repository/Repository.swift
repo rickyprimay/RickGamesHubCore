871ed78eeaf4db2fe613b0a469120e9b59a3076e
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2025-01-04 20:42:00 +0700

    fixing error on repository

--- Sources/RickGameHubCore/Data/Repository/Repository.swift
+++ Sources/RickGameHubCore/Data/Repository/Repository.swift
@@ -6,6 +6,7 @@
 //
 
 import Combine
+import Foundation
 
 public protocol Repository {
     associatedtype Request
@@ -17,14 +18,11 @@ public protocol Repository {
 
 public extension Repository {
     func executePublisher(request: Request) -> AnyPublisher<Response, Error> {
-        // Default implementation: return an empty publisher
         return Empty<Response, Error>().eraseToAnyPublisher()
     }
     
     func executeFuture(request: Request) -> Future<Response, Error> {
-        // Default implementation: return a Future with a placeholder Response
         return Future { promise in
-            // Example implementation: You may want to customize this
             promise(.failure(NSError(domain: "DefaultFutureError", code: -1, userInfo: nil)))
         }
     }
